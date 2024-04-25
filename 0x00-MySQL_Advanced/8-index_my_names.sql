08acc029b68e8b3240809b3e80b1e78a7d99a2cd
Fadi Asaad <firon1222@gmail.com>
2024-04-26 01:59:11 +0300

    update task 8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,4 @@
+-- Drop index if it exists
+DROP INDEX idx_name_first IF EXISTS ON names;
 -- Create index for the first letter of the name using b-tree
 CREATE INDEX idx_name_first USING BTREE ON names(name(1));
