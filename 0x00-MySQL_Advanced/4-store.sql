7fa2f4ad7c31dcd4bf08f3c52b2fb09bd9f0a68d
Fadi Asaad <firon1222@gmail.com>
2024-04-25 23:02:45 +0200

    update task 4 : store sql script

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,6 @@
 -- Add a trigger to update stock on order
-
 -- drop trigger if exists
-DROP TRIGGER IF EXISTS update_stock_on_order;
-
+DROP TRIGGER IF EXISTS update_stock_trigger;
 -- create trigger
-CREATE TRIGGER update_stock_on_order
-  AFTER INSERT ON orders
-  FOR EACH ROW
-  BEGIN
-    UPDATE items SET quantity = quantity - NEW.number
-      WHERE name = NEW.item_name;
-  END;
+CREATE TRIGGER update_stock_trigger AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
