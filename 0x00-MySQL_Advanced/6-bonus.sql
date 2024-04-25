05d3940692488fefff1ba3305ed4c8cb706c3f5e
Fadi Asaad <firon1222@gmail.com>
2024-04-26 01:24:55 +0300

    motherfucker task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -6,17 +6,13 @@ CREATE PROCEDURE AddBonus (
   IN project_name VARCHAR(255),
   IN score FLOAT)
 BEGIN
-  -- get project id using its name
-  DECLARE project INT;
-  SET project = (SELECT id FROM projects WHERE name = project_name);
-  -- check if project is not exist
-  IF project = 0 THEN
-    -- insert new project with given name
+  -- insert project if not exists
+  IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 THEN
     INSERT INTO projects (name) VALUES (project_name);
-    -- get new project id
-    SET project = LAST_INSERT_ID();
   END IF;
 
+  -- get project id
+  SET @project_id = (SELECT id FROM projects WHERE name = project_name);
   -- insert new correction
-  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project, score);
+  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project_id, score);
 END
