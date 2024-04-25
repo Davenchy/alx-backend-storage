ee60e0e6095725e982ab019837a14499278cb035
Fadi Asaad <firon1222@gmail.com>
2024-04-26 01:16:18 +0300

    motherfucker task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,18 +1,17 @@
--- Delete bonus procedure if exists
-DROP PROCEDURE IF EXISTS AddBonus;
 -- Create AddBonus procedure
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
   -- get project id using its name
-  SET @project = (SELECT id FROM projects WHERE name = project_name);
+  DECLARE project INT;
+  SET project = (SELECT id FROM projects WHERE name = project_name);
   -- check if project is not exist
-  IF @project IS NULL THEN
+  IF project IS NULL THEN
     -- insert new project with given name
     INSERT INTO projects (name) VALUES (project_name);
     -- get new project id
-    SET @project = LAST_INSERT_ID();
+    SET project = LAST_INSERT_ID();
   END IF;
 
   -- insert new correction
-  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project, score);
+  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project, score);
 END
