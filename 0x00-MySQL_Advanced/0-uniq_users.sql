b01c0b7f259aa8416c8745a1d259384c4956127c
Fadi Asaad <firon1222@gmail.com>
2024-04-25 21:36:04 +0200

    fix the motherfucker task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,4 @@
--- creates a table users with unique email and name
--- in the database holberton
-CREATE DATABASE IF NOT EXISTS holberton;
--- use the database
-USE holberton;
-
--- create the table users
+-- Create users table
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
