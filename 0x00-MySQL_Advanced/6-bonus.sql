81664c3ec2c1eaae8eeda84b761cd01ed62c8ef5
Fadi Asaad <firon1222@gmail.com>
2024-04-26 01:13:30 +0300

    update task 6: create project if not exist

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -3,9 +3,16 @@ DROP PROCEDURE IF EXISTS AddBonus;
 -- Create AddBonus procedure
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-  DECLARE project VARCHAR(255);
-  SET project = (SELECT id FROM projects WHERE name = project_name);
-  IF project IS NOT NULL THEN
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project, score);
+  -- get project id using its name
+  SET @project = (SELECT id FROM projects WHERE name = project_name);
+  -- check if project is not exist
+  IF @project IS NULL THEN
+    -- insert new project with given name
+    INSERT INTO projects (name) VALUES (project_name);
+    -- get new project id
+    SET @project = LAST_INSERT_ID();
   END IF;
+
+  -- insert new correction
+  INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project, score);
 END
