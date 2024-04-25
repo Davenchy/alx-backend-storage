3a4130d693003f5dc752ccb1eef622477073bb08
Fadi Asaad <firon1222@gmail.com>
2024-04-26 01:03:24 +0300

    update task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,4 @@
 -- Create trigger to set user's email invalid on email update
--- drop trigger if exists
-DROP TRIGGER IF EXISTS update_email_trigger;
--- create trigger
 CREATE TRIGGER update_email_trigger BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
 IF NEW.email <> OLD.email THEN
