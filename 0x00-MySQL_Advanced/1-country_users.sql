24dceb593b65f6849233dfa22655bdbc4eb3eb8d
Fadi Asaad <firon1222@gmail.com>
2024-04-25 21:40:13 +0200

    add task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,6 @@
+-- Create users table with country enum column
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
