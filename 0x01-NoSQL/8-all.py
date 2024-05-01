43d4512b1fc01677292af18b52baae63b2a744d8
Fadi Asaad <firon1222@gmail.com>
2024-05-01 03:18:16 +0300

    update task 8: use up to date approatch

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
 """This module contains a function that lists all documents in a collection"""
 
 
 def list_all(mongo_collection):
     """lists all documents in a collection"""
+    if mongo_collection.count_documents({}) == 0:
+        return []
     return mongo_collection.find()
