d2fc1092a3ffe673fb562d9308f7c5e2b26eb185
Fadi Asaad <firon1222@gmail.com>
2024-05-01 04:46:00 +0300

    update task 101: use less stages with $project

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,10 +6,8 @@ that returns all students sorted by average score """
 def top_students(mongo_collection):
     """ returns all students sorted by average score """
     return mongo_collection.aggregate([
-        {'$unwind': '$topics'},
-        {'$group': {
-            '_id': '$_id',
-            'name': {'$first': '$name'},
+        {'$project': {
+            'name': 1,
             'averageScore': {'$avg': '$topics.score'}
         }},
         {'$sort': {'averageScore': -1}}
