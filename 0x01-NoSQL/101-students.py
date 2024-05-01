9305f997836a450c9fda08e2dba7b2bb9f16715d
Fadi Asaad <firon1222@gmail.com>
2024-05-01 05:37:06 +0300

    Revert "update task 101: use less stages with $project"
    This reverts commit d2fc1092a3ffe673fb562d9308f7c5e2b26eb185.

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,8 +6,10 @@ that returns all students sorted by average score """
 def top_students(mongo_collection):
     """ returns all students sorted by average score """
     return mongo_collection.aggregate([
-        {'$project': {
-            'name': 1,
+        {'$unwind': '$topics'},
+        {'$group': {
+            '_id': '$_id',
+            'name': {'$first': '$name'},
             'averageScore': {'$avg': '$topics.score'}
         }},
         {'$sort': {'averageScore': -1}}
