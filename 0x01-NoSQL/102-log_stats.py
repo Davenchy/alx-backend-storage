b02b7c3c1e8fab7f172a7e9b3f2f7c4974ce604a
Fadi Asaad <firon1222@gmail.com>
2024-05-01 05:40:51 +0300

    update task 102: revert to old solution

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ This script logs statistics about Nginx logs stored in MongoDB """
 from pymongo import MongoClient
-from pprint import pprint
 
 
 METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
@@ -10,49 +9,24 @@ if __name__ == "__main__":
     with MongoClient() as client:
         docs = client.logs.nginx
 
-        def countMethod(methodName):
-            return {
-                '$sum': {
-                    '$cond': [{'$eq': ['$method', methodName]}, 1, 0]
-                }
-            }
+        print(f'{docs.count_documents({})} logs')
+        print('Methods:')
 
-        results = docs.aggregate([
-            {
-                '$group': {
-                    '_id': None,
-                    'count': {'$sum': 1},
-                    'status': {
-                        '$sum': {
-                            '$cond': [{'$and': [
-                                {'$eq': ['$method', 'GET']},
-                                {'$eq': ['$path', '/status']}
-                            ], }, 1, 0]
-                        }
-                    },
-                    **{
-                        method: countMethod(method)
-                        for method in METHODS
-                    }
-                }
-            }
-        ]).next()
+        for method in METHODS:
+            count = docs.count_documents({"method": method})
+            print(f'\tmethod {method}: {count}')
 
-        log = "{logs} logs\nMethods:\n{methods}\n{status} status check\nIPs:"
-        print(log.format(
-            logs=results["count"],
-            methods="\n".join(
-                f"\tmethod {method}: {results.get(method, 0)}"
-                for method in METHODS
-            ),
-            status=results["status"]
-        ))
+        statusCount = docs.count_documents({
+            "method": "GET", "path": "/status"
+        })
+        print(f'{statusCount} status check')
+
+        print('IPs:')
 
         ips = docs.aggregate([
             {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
             {"$sort": {"count": -1}},
             {"$limit": 10}
         ])
-
         for ip in ips:
             print(f"\t{ip['_id']}: {ip['count']}")
