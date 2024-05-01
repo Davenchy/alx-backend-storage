1771ab0839c45ac57f59c3525201f151b1134b69
Fadi Asaad <firon1222@gmail.com>
2024-05-01 06:39:34 +0300

    add get, get_str and get_int to Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,10 @@
 """ Using redis in python """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+
+DataTypes = Union[str, bytes, int, float]
+ConversionFn = Callable[[bytes], DataTypes]
 
 
 class Cache:
@@ -13,8 +16,23 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: DataTypes) -> str:
         """ Cache/store data """
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> DataTypes:
+        """ get data from cache """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ get data from cache as string """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ get data from cache as integer """
+        return self.get(key, int)
