9c9df071799b500bc14e2802d96b54e84ee57bae
Fadi Asaad <firon1222@gmail.com>
2024-05-02 07:06:23 +0300

    fix task 100: my final solution and fuck this unsolvable project

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,31 +2,28 @@
 """ implement simple cache decorator using redis db """
 import requests
 import redis
-from time import sleep
+from functools import wraps
+from typing import Callable
 
 db = redis.Redis()
 
 
+def cache(method: Callable) -> Callable:
+    """ A decorator to cache get_page results """
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        """ wrapper function to cache get_page results """
+        db.incr(f'count:{url}')
+        content = db.get(f'content:{url}')
+        if content:
+            return content.decode('utf-8')
+        content = method(url)
+        db.setex(f'content:{url}', 10, content)
+        return content
+    return wrapper
+
+
+@cache
 def get_page(url: str) -> str:
     """ Fetch the content of a page using its URL """
-    # db.incr(f'count:{url}')
-    content = db.get(f'content:{url}')
-    if content:
-        return content.decode('utf-8')
-    content = requests.get(url).text
-    db.setex(f'content:{url}', 10, content)
-    return content
-
-
-if __name__ == '__main__':
-    db.flushdb()
-    URL = 'https://github.com'
-    get_page(URL)
-    sleep(2)
-    get_page(URL)
-    sleep(3)
-    get_page(URL)
-    sleep(5)
-    get_page(URL)
-    count = int(db.get('count:' + URL))
-    print(f'Visited {count} times')
+    return requests.get(url).text
